4e917bc3600c624da25132ea3541a908d4e03e5c
Andreas <andreas.siaplaouras@object.ch>
2023-10-24 16:23:25 +0200

    Add navbar

---
+++ views/src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+interface NavBarProps {
+  setPage: (page: string) => void;
+}
+export default function NavBar(props: NavBarProps) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{backgroundColor: 'white', color: 'black'}} position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Managment System
+          </Typography>
+          <Button color="inherit" onClick={() => props.setPage('customers')} >Customers</Button>
+          <Button color="inherit" onClick={() => props.setPage('offers')} >Offers</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
